c015e7857c605f06bcb9a3dc5c8e693fcf1efc65
Michael Henrique <oliveirasantos.mh@gmail.com>
2020-05-05 16:42:20 -0300

    feat: main function completed

--- image-transform/main.cpp
+++ image-transform/main.cpp
@@ -4,7 +4,7 @@
  *
  * @author University of Illinois CS 225 Course Staff
  * @author Updated by University of Illinois CS 400 Course Staff
-**/
+ **/
 
 #include "ImageTransform.h"
 #include "uiuc/PNG.h"
@@ -15,7 +15,7 @@ int main() {
   png.readFromFile("alma.png");
   result = grayscale(png);
   result.writeToFile("out-grayscale.png");
-  
+
   result = createSpotlight(png, 450, 150);
   result.writeToFile("out-spotlight.png");
 
@@ -25,6 +25,6 @@ int main() {
   png2.readFromFile("overlay.png");
   result = watermark(png, png2);
   result.writeToFile("out-watermark.png");
-  
+
   return 0;
 }
